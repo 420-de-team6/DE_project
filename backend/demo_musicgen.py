d6963626093a1395d634a4a9d95ad6078e6870d0
kyle <kyle@wrtn.io>
2024-04-27 23:20:18 +0900

    fix: model output

--- backend/demo_musicgen.py
+++ backend/demo_musicgen.py
@@ -1,36 +1,86 @@
+# %pip install transformers
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
 
 import scipy
 
 import librosa
 from pydub import AudioSegment
+import numpy as np
+import io
+from datasets import load_dataset
+from scipy.io import wavfile
+
+
+def convert_mp3_to_wav(input_file):
+    mp3 = AudioSegment.from_file(input_file, format="mp3")
+    buffer = io.BytesIO()
+    mp3.export(
+        buffer,
+        format="wav",
+    )
+    buffer.seek(0)
+    return buffer
+
+
+def load_wav_to_numpy(wav):
+    wav.seek(0)
+    y, sr = librosa.load(wav, sr=32000, dtype=np.float32)
+
+    return y, sr
 
 
 def generate_music(input_file, output_file, input_prompt, target_sample_rate=32000):
-    
+
     # 초기값
 
     # input_prompt = "jazzy track with groovy saxophone"
     # input_file = "newjeans.wav"
     # output_file = "output.wav"
     # target_sample_rate = 32000
-    
-    mp3 = AudioSegment.from_file(input_file, format='mp3')
-    wav = mp3.set_frame_rate(target_sample_rate).set_channels(1)
+
+    # buffer = io.BytesIO()
+
+    # wav = np.frombuffer(
+    #     mp3.set_frame_rate(target_sample_rate)
+    #     .set_channels(1)
+    #     .export(format="wav")
+    #     .read(),
+    # )
 
     # get pretrained model
 
     processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
     model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
 
+    # wav = convert_mp3_to_wav(input_file)
+    AudioSegment.from_file(input_file, format="mp3", sample_rate=32000).set_frame_rate(
+        32000
+    ).set_channels(1).export("temporary.wav", format="wav")
+    sample_rate, data = wavfile.read("temporary.wav")
+    print(sample_rate, data.shape)
+
+    # data, sample_rate = load_wav_to_numpy(wav)
+    # mp3 = AudioSegment.from_file(input_file, format="mp3")
+
+    # dataset = load_dataset("sanchit-gandhi/gtzan", split="train", streaming=True)
+    # sample = next(iter(dataset))["audio"]
+    # print(dataset)
+
+    # print(wav)
     # load audio file
 
-    data, sample_rate = librosa.load(wav, sr=target_sample_rate)
+    # data, sample_rate = librosa.load(wav, sr=target_sample_rate)
 
     # generate audio
+    # print(sample)
+    # print(sample["array"].shape)
+    # print(data, sample_rate)
+    # print(data.shape)
+
+    data = np.array(data, copy=False).astype("float32")
 
     inputs = processor(
-        audio=data,
+        audio=data[:30000],
         sampling_rate=sample_rate,
         text=[input_prompt],
         padding=True,
@@ -42,7 +92,9 @@ def generate_music(input_file, output_file, input_prompt, target_sample_rate=320
 
     # generate wave audio file
     sampling_rate = model.config.audio_encoder.sampling_rate
-    scipy.io.wavfile.write(output_file, rate=sampling_rate, data=audio_values[0, 0].numpy())
+    scipy.io.wavfile.write(
+        output_file, rate=sampling_rate, data=audio_values[0, 0].numpy()
+    )
     return output_file
     # sampling_rate = model.config.audio_encoder.sampling_rate
     # Audio(audio_values[0].numpy(), rate=sampling_rate)
--- backend/main.py
+++ backend/main.py
@@ -6,20 +6,27 @@ from test import get_audio_file, find_path
 from classification import clf
 from demo_musicgen import generate_music
 import pygame
+
 app = FastAPI()
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 모든 origin을 허용하려면 "*"을 사용합니다. 필요에 따라 원하는 origin을 명시할 수 있습니다.
+    allow_origins=[
+        "*"
+    ],  # 모든 origin을 허용하려면 "*"을 사용합니다. 필요에 따라 원하는 origin을 명시할 수 있습니다.
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],  # 허용할 HTTP 메서드를 지정합니다.
-    allow_headers=["*"],  # 모든 헤더를 허용하려면 "*"을 사용합니다. 필요에 따라 원하는 헤더를 명시할 수 있습니다.
+    allow_headers=[
+        "*"
+    ],  # 모든 헤더를 허용하려면 "*"을 사용합니다. 필요에 따라 원하는 헤더를 명시할 수 있습니다.
 )
 pygame.mixer.init()
+
+
 @app.get("/")
 async def root():
-    print('success')
+    print("success")
     return {"message": "Hello World"}
 
 
@@ -31,7 +38,7 @@ async def mix_music(selected_files: dict):
     file_name1, file_name2 = get_audio_file(selectedFile1, selectedFile2)
     paths = [file_name1, file_name2]
     print(clf(paths)[0], type(clf(paths)[1]))
-    audio_file=generate_music(file_name1, 'output.wav', clf(paths)[1], 32000)
+    audio_file = generate_music(file_name1, "output.wav", clf(paths)[1], 32000)
     pygame.mixer.music.load(audio_file)
     pygame.mixer.music.play()
     return JSONResponse(content={"message": "Audio files received successfully"})
